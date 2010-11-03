6f2539f046eb228db654586113cee8e963260f21
Gavin Mulligan <d3r1v3d@gmail.com>
2010-11-03 15:05:49 -0600

    Cleaned up styles for forms and associated validations. Much more intuitive. Also, added HAML helper to generate form fields.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+    def render_field(label, field, errors, description='')
+        #.label_container
+        #   %h3= label
+        #.field_container
+        #   .field
+        #       = field
+        #   .error_explanation
+        #        %p= resource.errors[:name].join(', ') : ''
+        #   .clear
+        haml_tag 'div.label_container' do
+            haml_tag :h3, label
+            haml_tag :p, '(' + description + ')' if !description.blank?
+        end
+        haml_tag 'div.field_container' do
+            haml_tag 'div.field', field
+            if !errors.blank?
+                haml_tag 'div.error_explanation' do
+                    haml_tag :p, errors.join(', ')
+                end
+            end
+            haml_tag 'div.clear'
+        end
+    end
 end
